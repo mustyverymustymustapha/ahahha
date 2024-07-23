14eaa5eff5f6b8cee42b9e981acc26860a26b805
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-23 08:57:47 +0000

    First

---
+++ script.js
@@ -0,0 +1,7 @@
+const languages = [
+  "JavaScript", "Python", "Java", "C++", "Ruby",
+  "Go", "Swift", "Kotlin", "TypeScript", "Rust",
+  "PHP", "C#", "Scala", "Dart", "Haskell",
+  "Lua", "R", "Julia", "Erlang", "Clojure",
+  "F#", "Elixir", "Groovy", "Perl", "COBOL"
+];
