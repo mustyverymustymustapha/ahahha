2d1b97efe63920c85fd8417229f2544083594b38
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-23 10:45:34 +0000

    Second

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ const languages = [
   "Lua", "R", "Julia", "Erlang", "Clojure",
   "F#", "Elixir", "Groovy", "Perl", "COBOL"
 ];
+
+const button = document.getElementById('languageButton');
+const result = document.getElementById('result');
+
+button.addEventListener('click', () => {
+ 
+  const randomIndex = Math.floor(Math.random() * languages.length);
+ 
+  const chosenLanguage = languages[randomIndex];
+ 
+  result.textContent = chosenLanguage;
+});
+// im really sorry this took a while it was hard because my brain has been tired the past few days
